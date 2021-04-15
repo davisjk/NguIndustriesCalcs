98c9200b4ad7d58786b5085c3e70682e1406e116
Jacob Davis <jamminjaminjams@gmail.com>
2021-04-15 00:52:37 -0600

    IDK what I was doing with box_counts and knight_counts but they're gone now fixing some problems. Added verbosity opt instead of having a bunch of commented out debug logs. Instead of looping over every space, finding the subset of spaces, then looping over those, it now finds the subsets once per file and also reduces them by removing supersets of other sets :D

--- OptimizeBeacons.py
+++ OptimizeBeacons.py
@@ -31,13 +31,16 @@ class NguiBeaconOptimizer:
 
   def __init__(self):
     self._parse_args()
-    # CAUTION, commented out debug messages create multiple GB of logs and drastically slow performance
     handlers=[logging.StreamHandler(sys.stdout)]
-    if self.args['l']:
-      handlers.append(logging.FileHandler(f"{'_'.join(file.split('.', 1)[0] for file in self.args['files'])}-{''.join([arg for arg in self.args if len(arg) == 1 and self.args[arg]])}_{round(time())}.log"))
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)-5s %(message)s', handlers=handlers)
+    if self.args.l:
+      handlers.append(logging.FileHandler(f"{'_'.join(file.split('.', 1)[0] for file in self.args.files)}-{''.join([arg for arg in self.args if len(arg) == 1 and self.args[arg]])}_{round(time())}.log"))
+    if self.args.verbosity > 0:
+      level = logging.DEBUG
+    else:
+      level = logging.INFO
+    logging.basicConfig(level=level, format='%(asctime)s %(levelname)-5s %(message)s', handlers=handlers)
     self.logger = logging.getLogger()
-    self.logger.debug(self.args)
+    self.logger.debug(vars(self.args))
     self._setup()
 
   def _parse_args(self):
@@ -52,37 +55,39 @@ class NguiBeaconOptimizer:
     parser.add_argument('-l', help='write to log file', action='store_true')
     parser.add_argument('-o', help='write to output file', action='store_true')
     parser.add_argument('-m', '--max', help='maximum permutations', type=int, default=0)
+    parser.add_argument('-v', '--verbosity', help='0 to 5 where 0 is only INFO logging and higher than 1 can potentially create millions of logs and affect performance', action='count', default=0)
     parser.add_argument('files', nargs='*')
-    self.args = vars(parser.parse_args())
+    self.args = parser.parse_args()
 
   def _setup(self):
+    # beacons must be ordered and empty must be first
     self.beacons = [self.empty]
-    self.logger.info(f'Empty = {self.empty}')
+    self.logger.debug(f'Empty = {self.empty}')
     self.filename_id = ''
-    if self.args['b']:
-      if self.args['x']:
-        self.beacons += [self.bx]
+    if self.args.b:
+      if self.args.x:
+        self.beacons.append(self.bx)
         self.filename_id += '_bx'
-        self.logger.info(f'Blue Box = {self.bx}')
-      if self.args['k']:
-        self.beacons += [self.bk]
+        self.logger.debug(f'Blue Box = {self.bx}')
+      if self.args.k:
+        self.beacons.append(self.bk)
         self.filename_id += '_bk'
-        self.logger.info(f'Blue Knight = {self.bk}')
-    if self.args['p']:
-      if self.args['x']:
-        self.beacons += [self.px]
+        self.logger.debug(f'Blue Knight = {self.bk}')
+    if self.args.p:
+      if self.args.x:
+        self.beacons.append(self.px)
         self.filename_id += '_px'
-        self.logger.info(f'Pink Box = {self.px}')
-      if self.args['k']:
-        self.beacons += [self.pk]
+        self.logger.debug(f'Pink Box = {self.px}')
+      if self.args.k:
+        self.beacons.append(self.pk)
         self.filename_id += '_pk'
-        self.logger.info(f'Pink Knight = {self.pk}')
+        self.logger.debug(f'Pink Knight = {self.pk}')
 
   def print_beacon_layouts(self):
-    for filename in self.args['files']:
-      if self.args['a']:
+    for filename in self.args.files:
+      if self.args.a:
         for combo in self.all_combos:
-          self.args['b'], self.args['p'], self.args['x'], self.args['k'] = combo
+          self.args.b, self.args.p, self.args.x, self.args.k = combo
           self._setup()
           self._find_best_layout(filename)
       else:
@@ -105,29 +110,34 @@ class NguiBeaconOptimizer:
   def _find_best_layout(self, filename):
     self.logger.info(f'Finding best layout for {filename} with beacons: {self.beacons}')
     start_time = round(time(), 3)
+    # get layout info that doesn't change per file
     output_filename = '{0}{2}.{1}'.format(*filename.split('.', 1), self.filename_id)
-    permutations = 0
     base_layout = self._read_file(filename)
     base_value = self._layout_value(base_layout)
     spaces = sum([row.count(self.empty) for row in base_layout])
-    self.logger.info(f'Starting layout value: {base_value}')
-    box_counts = []
-    knight_counts = []
+    subsets = []
     for y in range(0, len(base_layout)):
-      box_counts.append([])
-      knight_counts.append([])
       for x in range(0, len(base_layout[y])):
         if base_layout[y][x] in self.beacons:
-          box_counts[y].append(self._box_touching_count(base_layout, x, y))
-          knight_counts[y].append(self._knight_touching_count(base_layout, x, y))
-        else:
-          box_counts[y].append(0)
-          knight_counts[y].append(0)
-    # self.logger.debug(f'Starting layout:\n{self._layout_to_string(base_layout)}')
-    # self.logger.debug(f'Box counts:\n{self._layout_to_string(box_counts)}')
-    # self.logger.debug(f'Knight counts:\n{self._layout_to_string(knight_counts)}')
+          subsets.append(self._find_space_subset(base_layout, x, y))
+    # remove supersets of subsets to remove redundant calculations later
+    # removing the subsets instead introduces a logic bug
+    subsets_cp = subsets.copy()
+    for subset in subsets:
+      for other in subsets_cp:
+        if subset < other:
+          subsets_cp.remove(other)
+          # subsets_cp.remove(subset)
+          # break
+    subsets = subsets_cp
+    if self.args.verbosity >= 2:
+      self.logger.debug(f'Starting layout value: {base_value}')
+      self.logger.debug(f'Starting layout:\n{self._layout_to_string(base_layout)}')
+      self.logger.debug(f'{len(subsets)} subsets: {subsets}')
+    permutations = 0
     best_layout = base_layout
     best_value = base_value
+    percent_bonus = 0
     # Intent:
     # - loop through every valid space
     # - find every space that affects this space
@@ -137,22 +147,21 @@ class NguiBeaconOptimizer:
     # - loop again until the new best value and the old best value are the same
     while True:
       permutations += 1
-      # self.logger.debug(f'Starting permutation {permutations}')
+      if self.args.verbosity >= 3:
+        self.logger.debug(f'Starting permutation {permutations}')
       layout = deepcopy(best_layout)
       value = best_value
-      for y in range(0, len(layout)):
-        for x in range(0, len(layout[y])):
-          if layout[y][x] in self.beacons:
-            #TODO, make a list of all sublayouts and skip any that are subsets of others
-            layout, value = self._find_best_sublayout(layout, x, y, box_counts, knight_counts)
-            # self.logger.debug(f'Layout after permutation {permutations} and subset of {x}, {y}:\n{self._layout_to_string(layout)}')
+      for subset in subsets:
+        layout, value = self._find_best_sublayout(layout, value, subset)
       if value > best_value:
         best_layout = deepcopy(layout)
         best_value = value
-        self.logger.debug(f'New best layout at permutation {permutations}: {best_value}')
-        # self.logger.debug('\n' + self._layout_to_string(best_layout))
+        if self.args.verbosity >= 2:
+          self.logger.debug(f'New best layout at permutation {permutations}: {best_value}')
+        if self.args.verbosity >= 3:
+          self.logger.debug('\n' + self._layout_to_string(best_layout))
         percent_bonus = round((100 * best_value / spaces) - 100, 2)
-        if self.args['o']:
+        if self.args.o:
           extra_contents = [
             f'CAUTION: This may not be fully optimized, it is output after permutation {permutations}',
             f'Effective production: {best_value}',
@@ -160,9 +169,9 @@ class NguiBeaconOptimizer:
           ]
           self._write_file(output_filename, best_layout, extra_contents)
       elif value == best_value:
-        self.logger.debug(f'Found best layout after {permutations} permutations')
-        # self.logger.debug(f'Layout == base layout: {layout == best_layout}')
-        if self.args['o']:
+        if self.args.verbosity >= 1:
+          self.logger.debug(f'Found best layout after {permutations} permutations')
+        if self.args.o:
           extra_contents = [
             f'Effective production: {best_value}',
             f'Effective bonus to production without beacons: {percent_bonus}%'
@@ -176,64 +185,50 @@ class NguiBeaconOptimizer:
         self.logger.fatal(f'Current value: {value}')
         self.logger.fatal(f'Current layout:\n{self._layout_to_string(layout)}')
         sys.exit(1)
-      if self.args['max'] > 0 and permutations >= self.args['max']:
+      if self.args.max > 0 and permutations >= self.args.max:
         self.logger.info(f'Stopped optimizing after {permutations} permutations upon reaching maximum number of permutations')
         break
     stop_time = round(time(), 3)
-    self.logger.info(f'Optimized beacon layout of {filename} found in {round(stop_time - start_time, 3)} seconds with beacons: {self.beacons}')
+    self.logger.info(f'Optimized beacon placement of {filename} found in {round(stop_time - start_time, 3)} seconds with beacons: {self.beacons}')
     self.logger.info(f'Effective bonus to base production with this layout is {percent_bonus}% with a total production of {best_value}')
     self.logger.info('\n' + self._layout_to_string(best_layout))
 
-  def _find_best_sublayout(self, layout, x, y, box_counts, knight_counts):
-    spaces = self._find_space_subset(layout, x, y)
-    value = self._layout_value(layout)
-    # self.logger.debug(f'Finding best sublayout for {spaces} with starting value: {value}')
-    best_layout, best_value, permutations = self._recurse_sublayout(layout, spaces.copy(), deepcopy(layout), value, 0, box_counts, knight_counts)
-    self.logger.debug(f'Found best sublayout after {permutations} permutations: {best_value}')
+  def _find_best_sublayout(self, layout, value, subset):
+    if self.args.verbosity >= 3:
+      self.logger.debug(f'Finding best sublayout for {subset} with starting value: {value}')
+    best_layout, best_value, subpermutations = self._recurse_sublayout(layout, subset.copy(), deepcopy(layout), value, 0)
+    if self.args.verbosity >= 2:
+      self.logger.debug(f'Found best sublayout after {subpermutations} subpermutations: {best_value}')
     return best_layout, best_value
 
-  def _find_space_subset(self, layout, x, y):
-    spaces = []
-    for k in range(-2, 3):
-      if y+k >= 0 and y+k < len(layout):
-        for j in range(-2, 3):
-          if x+j >= 0 and x+j < len(layout[y+k]):
-            if (not self.args['k'] and (abs(j) == 2 or abs(k) == 2)) or \
-               (not self.args['x'] and (j, k) != (0, 0) and abs(j) < 2 and abs(k) < 2) or \
-               abs(j) == abs(k) == 2 or \
-               (abs(j), k) == (2, 0) or \
-               (j, abs(k)) == (0, 2):
-              continue
-            else:
-              if layout[y+k][x+j] in [self.empty] + self.beacons:
-                spaces.append((x+j, y+k))
-    return spaces
-
-  def _recurse_sublayout(self, layout, spaces, best_layout, best_value, permutations, box_counts, knight_counts):
-    #TODO, looks like there's still a bug in here
-    #see 3x4-alo_1618379690.log and 3x4_bx_px.txt compare 3x4_bx_bk_px_pk.txt
-    if spaces:
-      space = spaces.pop()
-      counterproductive = self._counterproductive_beacon(layout, *space, box_counts, knight_counts)
+  def _recurse_sublayout(self, layout, subset, best_layout, best_value, subpermutations):
+    if subset:
+      space = subset.pop()
+      counterproductive = self._counterproductive_beacon(layout, *space)
       for beacon in self.beacons:
-        # self.logger.debug(f'Checking {beacon} in {space}')
+        if self.args.verbosity >= 3:
+          self.logger.debug(f'Checking {beacon} in {space}')
         if beacon == self.empty or not counterproductive:
-          if (beacon == self.bx and box_counts[space[1]][space[0]] < self.bx_threshold) or \
-             (beacon == self.px and box_counts[space[1]][space[0]] < self.px_threshold) or \
-             (beacon == self.bk and knight_counts[space[1]][space[0]] < self.bk_threshold) or \
-             (beacon == self.pk and knight_counts[space[1]][space[0]] < self.pk_threshold):
+          if (beacon == self.bx and self._box_touching_count(layout, *space) < self.bx_threshold) or \
+             (beacon == self.px and self._box_touching_count(layout, *space) < self.px_threshold) or \
+             (beacon == self.bk and self._knight_touching_count(layout, *space) < self.bk_threshold) or \
+             (beacon == self.pk and self._knight_touching_count(layout, *space) < self.pk_threshold):
             continue
           layout[space[1]][space[0]] = beacon
-          best_layout, best_value, permutations = self._recurse_sublayout(deepcopy(layout), deepcopy(spaces), best_layout, best_value, permutations, box_counts, knight_counts)
+          if self.args.verbosity >= 3:
+            self.logger.debug(f'Current value: {self._layout_value(layout)}')
+            self.logger.debug(f'Current layout:\n{self._layout_to_string(layout)}')
+          best_layout, best_value, subpermutations = self._recurse_sublayout(deepcopy(layout), deepcopy(subset), best_layout, best_value, subpermutations)
     value = self._layout_value(layout)
     if value > best_value:
       best_layout = deepcopy(layout)
       best_value = value
-      # self.logger.debug(f'New best sublayout at permutation {permutations}: {value}')
-      # self.logger.debug('\n' + self._layout_to_string(best_layout))
-    return best_layout, best_value, permutations + 1
+      if self.args.verbosity >= 3:
+        self.logger.debug(f'New best sublayout at subpermutation {subpermutations}: {value}')
+        self.logger.debug('\n' + self._layout_to_string(best_layout))
+    return best_layout, best_value, subpermutations + 1
 
-  def _counterproductive_beacon(self, layout, x, y, box_counts, knight_counts):
+  def _counterproductive_beacon(self, layout, x, y):
     adjustment = 1 if layout[y][x] == self.empty else 0
     for k in range(-2, 3):
       if y+k >= 0 and y+k < len(layout):
@@ -246,15 +241,15 @@ class NguiBeaconOptimizer:
               continue
             # knights
             elif abs(j) == 2 or abs(k) == 2:
-              if layout[y+k][x+j] == self.bk and knight_counts[y+k][x+j] - adjustment < self.bk_threshold:
+              if layout[y+k][x+j] == self.bk and self._knight_touching_count(layout, x+j, y+k) - adjustment < self.bk_threshold:
                 return True
-              elif layout[y+k][x+j] == self.pk and knight_counts[y+k][x+j] - adjustment < self.pk_threshold:
+              elif layout[y+k][x+j] == self.pk and self._knight_touching_count(layout, x+j, y+k) - adjustment < self.pk_threshold:
                 return True
             # boxes
             else:
-              if layout[y+k][x+j] == self.bx and box_counts[y+k][x+j] - adjustment < self.bx_threshold:
+              if layout[y+k][x+j] == self.bx and self._box_touching_count(layout, x+j, y+k) - adjustment < self.bx_threshold:
                 return True
-              elif layout[y+k][x+j] == self.px and box_counts[y+k][x+j] - adjustment < self.px_threshold:
+              elif layout[y+k][x+j] == self.px and self._box_touching_count(layout, x+j, y+k) - adjustment < self.px_threshold:
                 return True
     return False
 
@@ -282,12 +277,29 @@ class NguiBeaconOptimizer:
               count += 1
     return count
 
+  def _find_space_subset(self, layout, x, y):
+    subset = set()
+    for k in range(-2, 3):
+      if y+k >= 0 and y+k < len(layout):
+        for j in range(-2, 3):
+          if x+j >= 0 and x+j < len(layout[y+k]):
+            if (not self.args.k and (abs(j) == 2 or abs(k) == 2)) or \
+               (not self.args.x and (j, k) != (0, 0) and abs(j) < 2 and abs(k) < 2) or \
+               abs(j) == abs(k) == 2 or \
+               (abs(j), k) == (2, 0) or \
+               (j, abs(k)) == (0, 2):
+              continue
+            else:
+              if layout[y+k][x+j] in self.beacons:
+                subset.add((x+j, y+k))
+    return subset
+
   def _layout_value(self, layout):
     total = 0
     for y in range(0, len(layout)):
       for x in range(0, len(layout[y])):
         # [bx, bk, px, pk]
-        beacons = [0, 0, 0, 0]
+        beacon_counts = [0, 0, 0, 0]
         if layout[y][x] == self.empty:
           for k in range(-2, 3):
             if y+k >= 0 and y+k < len(layout):
@@ -301,24 +313,26 @@ class NguiBeaconOptimizer:
                   # knights
                   if abs(j) == 2 or abs(k) == 2:
                     if layout[y+k][x+j] == self.bk:
-                      beacons[1] += 1
+                      beacon_counts[1] += 1
                     elif layout[y+k][x+j] == self.pk:
-                      beacons[3] += 1
+                      beacon_counts[3] += 1
                   # boxes
                   else:
                     if layout[y+k][x+j] == self.bx:
-                      beacons[0] += 1
+                      beacon_counts[0] += 1
                     elif layout[y+k][x+j] == self.px:
-                      beacons[2] += 1
-          value = self._space_value(beacons)
-          # self.logger.debug(f'x: {x-2}, y: {y-2} affected by {beacons} with effective productivity: {value}')
+                      beacon_counts[2] += 1
+          value = self._space_value(beacon_counts)
+          if self.args.verbosity >= 5:
+            self.logger.debug(f'x: {x-2}, y: {y-2} affected by {beacon_counts} with effective productivity: {value}')
           total += value
-    # self.logger.debug(f'Total: {total}')
-    # self.logger.debug('\n' + self._layout_to_string(layout))
+    if self.args.verbosity >= 4:
+      self.logger.debug(f'Total: {total}')
+      self.logger.debug('\n' + self._layout_to_string(layout))
     return round(total, 2)
 
-  def _space_value(self, beacons):
-    return (1 + (self.bx_bonus * beacons[0]) + (self.bk_bonus * beacons[1])) * (1 + (self.px_bonus * beacons[2]) + (self.pk_bonus * beacons[3]))
+  def _space_value(self, beacon_counts):
+    return (1 + (self.bx_bonus * beacon_counts[0]) + (self.bk_bonus * beacon_counts[1])) * (1 + (self.px_bonus * beacon_counts[2]) + (self.pk_bonus * beacon_counts[3]))
 
 if __name__ == '__main__':
   mapper = NguiBeaconOptimizer()
